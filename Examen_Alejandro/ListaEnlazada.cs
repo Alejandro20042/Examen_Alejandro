a22c169770263b3bdf7758fab6aaf271258a1a66
Alejandro20042 <alejandrosillasalcantar.com@gmail.com>
2023-12-07 10:04:46 -0700

    Agregar archivos de proyecto.

---
+++ Examen_Alejandro/ListaEnlazada.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_Alejandro
+{
+    class Producto
+    {
+        public string Nombre { get; set; }
+        public int Precio { get; set; }
+
+        public Producto(string nombre, int precio) 
+        {
+            Nombre = nombre;
+            Precio = precio;
+        
+        }
+    }
+
+    class Nodo
+    {
+        public Producto Dato { get; set; }
+        public Nodo Siguiente { get; set; }
+    }
+    
+    internal class ListaEnlazada
+    {
+        private Nodo cabeza;
+
+        public void AgregarProducto(Producto producto)
+        {
+            Nodo nuevoNodo = new Nodo { Dato = producto, Siguiente = null};
+
+            if(cabeza == null)
+            {
+                cabeza = nuevoNodo;
+            }
+            Nodo actual = cabeza;
+            while (actual.Siguiente != null)
+            {
+                actual = actual.Siguiente;
+            }
+            actual.Siguiente = nuevoNodo;
+
+        }
+
+        public int BuscarProducto(int precio)
+        {
+            Nodo actual = cabeza;
+            int posicion = 0;
+            if (posicion != null) 
+            {
+                while (actual.Dato.Precio == precio )
+                {
+                    Console.WriteLine($"El producto {actual.Dato.Precio}: del {actual.Dato.Nombre} esta en la posicion: {posicion}");
+                }
+                actual = actual.Siguiente;
+                posicion++;
+            }
+            return -1;
+        }
+
+        public void MostrarProductos()
+        {
+            Nodo actual = cabeza;
+            while (actual != null)
+            {
+                Console.WriteLine($"Nombre: {actual.Dato.Nombre}, Precio: {actual.Dato.Precio}");
+                actual = actual.Siguiente;
+            }
+        }
+    }
+}
---
+++ Examen_Alejandro/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Examen_Alejandro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ListaEnlazada listaEnlazada = new ListaEnlazada();
+
+            
+                while (true)
+                {
+                   
+
+                    Console.WriteLine("1.- Agregar Producto");
+                    Console.WriteLine("2.- Buscar Producto");
+                    Console.WriteLine("3.- Imprimir Productos");
+
+
+                //int opciociones = int.Parse(Console.ReadLine());
+                 int opcion = Convert.ToInt32(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        Console.WriteLine("Ingresa el nombre del producto");
+                        string? nombre = Console.ReadLine();
+                        Console.WriteLine("Ingresa el precio del producto");
+                        int precio = int.Parse(Console.ReadLine());
+
+                        Producto productoNuevo = new Producto(nombre, precio);
+
+                        listaEnlazada.AgregarProducto(productoNuevo);
+
+                        break;
+                    case 2:
+                        Console.WriteLine("Buscar Producto: ");
+
+                        int nombreProducto = int.Parse(Console.ReadLine());
+                       listaEnlazada.BuscarProducto(nombreProducto);
+                        break;
+                    case 3:
+                        listaEnlazada.MostrarProductos();
+                        break;
+                }
+
+                    
+                }
+               
+        }
+    }
+}
